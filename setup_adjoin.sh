9cd99c2a7a2bb5b8240eaa3043fa4f9fc7e55ea0
tin6150 <tin6150@gmail.com>
2019-09-08 21:18:58 -0700

    compiling openmpi 2.1.6 into /opt/openmpi/2.1.6 [bofh]

--- setup_adjoin.sh
+++ setup_adjoin.sh
@@ -15,8 +15,10 @@ cd CMAQ-4.5-ADJ-LAJB_tutorial/code/CMAQ-4.5-ADJ-LAJB
 cd built_gcc_gfortran_serial_BENZ
 source environment-user				## ie source environment-user in the "build" dir, not the higher level dir I used earlier.
 
-make dir
+make dir							## this isn't relative.  it is hard coded to use path in var DIR_REPO or something.
 make pario
+
+## not sure if there rest need to cd to a diff folder... 
 
 make stenex
 make jproc
